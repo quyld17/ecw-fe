88d52fadc7dfaf7c263c8890852be77696acad41
Lê Duy Quý <quyld17@gmail.com>
2025-05-31 10:42:30 +0700

    fix

--- src/app/user/profile/page.js
+++ src/app/user/profile/page.js
@@ -74,7 +74,6 @@ export default function Profile() {
         setAddresses([]);
       }
     } catch (error) {
-      console.error("Error fetching addresses:", error);
       setAddresses([]);
     }
   };
